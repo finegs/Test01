7d49a8f434e8e8dc753872bfe888866e30f30824
finegs <finegs@gmail.com>
2019-05-17 11:11:08 +0900

    ...

---
+++ pjt01/u.hpp
@@ -0,0 +1,48 @@
+
+#ifndef __U_HPP
+#define __U_HPP
+
+#define INITIAL_SIZE 1024
+
+void DieWithError(const char* errorMessage);
+
+struct Buffer
+{
+		void* data;
+		int next;
+		size_t size;
+};
+
+struct Buffer* new_buffer(size_t data_len = INITIAL_SIZE);
+void reserve_space(Buffer* b, size_t bytes);
+void serialize_int(int x, Buffer* b);
+
+class Packet
+{
+	public:
+		static const size_t MaxDataSize;
+		int senderId;
+		int sequenceNumber;
+		char* data;
+
+	public:
+		Packet();
+		~Packet();
+
+	public:
+		void* serialize();
+		void deserialize(void* message);
+};
+
+struct SerializedPacket {
+	public:
+		int senderId;
+		int sequenceNumber;
+		char *data;
+	public:
+		SerializedPacket();
+		~SerializedPacket();
+} __attribute__((packed));
+
+
+#endif
