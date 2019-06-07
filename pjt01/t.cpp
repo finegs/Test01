43b64eac82e0390c4f2356fe0a150fd5be3b327f
finegs <finegs@gmail.com>
2019-06-07 22:37:07 +0900

    boost::iterprocess added

--- pjt01/t.cpp
+++ pjt01/t.cpp
@@ -7,6 +7,82 @@
 #include <sstream>
 #include <utility>
 #include <vector>
+#include <functional>
+
+#ifndef __MY_IPC
+#define __MY_IPC
+
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/containers/map.hpp>
+#include <boost/interprocess/allocators/allocator.hpp>
+
+class MyIPC {
+	public:
+		static int testIPC();
+};
+
+int MyIPC::testIPC() {
+   using namespace boost::interprocess;
+
+   //Remove shared memory on construction and destruction
+   struct shm_remove
+   {
+      shm_remove() { shared_memory_object::remove("MySharedMemory"); }
+      ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); }
+   } remover;
+
+   //Shared memory front-end that is able to construct objects
+   //associated with a c-string. Erase previous shared memory with the name
+   //to be used and create the memory segment at the specified address and initialize resources
+   managed_shared_memory segment
+      (create_only
+      ,"MySharedMemory" //segment name
+      ,65536);          //segment size in bytes
+
+   //Note that map<Key, MappedType>'s value_type is std::pair<const Key, MappedType>,
+   //so the allocator must allocate that pair.
+   typedef int    KeyType;
+   typedef float  MappedType;
+   typedef std::pair<const int, float> ValueType;
+
+   //Alias an STL compatible allocator of for the map.
+   //This allocator will allow to place containers
+   //in managed shared memory segments
+   typedef allocator<ValueType, managed_shared_memory::segment_manager>
+      ShmemAllocator;
+
+   //Alias a map of ints that uses the previous STL-like allocator.
+   //Note that the third parameter argument is the ordering function
+   //of the map, just like with std::map, used to compare the keys.
+   typedef map<KeyType, MappedType, std::less<KeyType>, ShmemAllocator> MyMap;
+
+   //Initialize the shared memory STL-compatible allocator
+   ShmemAllocator alloc_inst (segment.get_segment_manager());
+
+   //Construct a shared memory map.
+   //Note that the first parameter is the comparison function,
+   //and the second one the allocator.
+   //This the same signature as std::map's constructor taking an allocator
+   MyMap *mymap =
+      segment.construct<MyMap>("MyMap")      		//object name
+                                 (std::less<int>() //first  ctor parameter
+                                 ,alloc_inst);     //second ctor parameter
+
+   //Insert data in the map
+   for(int i = 0; i < 100; ++i){
+      mymap->insert(std::pair<const int, float>(i, (float)i));
+   }
+
+
+   // Get Data in the map
+   for(int i = 0;i < 100;++i) {
+	//   std::cout << "mymap[" << i << "] : " << mymap->find(i)->first << std::endl;
+	   std::cout << "mymap[" << i << "] : " << (*mymap)[i] << std::endl;
+   }
+   return 0;	
+}
+
+#endif
 
 #include <u.hpp>
 
@@ -61,6 +137,7 @@ class MyClz {
             }
         };
 };
+
 
 std::unordered_map<std::string, MyClz> m1;
 
@@ -202,6 +279,16 @@ int main(int argc, char* argv[]) {
 		else if("-h" == cmd || "-H" == cmd || "-help" == cmd|| "-HELP" == cmd) {
 			MyClz::printCRUDUsage();
 		}
+		else if("-t" == cmd || "-T" == cmd) {
+			std::cout << "IPC Test : " << "MyIPC::testIPC" << std::endl;
+			MyIPC::testIPC();
+
+//			int i = 0;
+//			std::cout << "params.size = " << params.size() << std::endl;
+//			for(const auto& iter = params.cbegin(); iter != paarams.cend();iter++) {
+//				std::cout << "params[" << i++ << "] = " <<  params[i] << std::endl;
+//			}
+		}
 		else {
 			std::cout << "\t>> [E] Unsupported command : " << key << std::endl;
 			MyClz::printCRUDUsage();
@@ -226,7 +313,7 @@ int main(int argc, char* argv[]) {
 #include <vector>
 #include <initializer_list>
  
-template <class T>
+emplate <class T>
 struct S {
     std::vector<T> v;
     S(std::initializer_list<T> l) : v(l) {
