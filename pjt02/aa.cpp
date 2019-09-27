adaee06c476cac5a997369594723f700920d0039
KANGSOONGOO <finegs@gmail.com>
2019-09-27 21:08:58 +0900

    capture example in aa.cpp

--- pjt02/aa.cpp
+++ pjt02/aa.cpp
@@ -13,10 +13,10 @@ void log(const std::chrono::system_clock::time_point& tp, const std::string& msg
 {
     std::chrono::system_clock::duration se = tp.time_since_epoch();
     std::time_t nc = std::chrono::system_clock::to_time_t(tp);
-//    char a[3+1];
-//   sprintf(a, "%03lld", 
-//            std::chrono::duration_cast<std::chrono::milliseconds>(se - std::chrono::duration_cast<std::chrono::seconds>(se)));
-//    std::cout << std::put_time(std::localtime(&nc), "%F %T.") << a << " : " << msg << std::endl;
+    char a[3+1];
+	sprintf(a, "%03lld", 
+           std::chrono::duration_cast<std::chrono::milliseconds>(se - std::chrono::duration_cast<std::chrono::seconds>(se)));
+    std::cout << std::put_time(std::localtime(&nc), "%F %T.") << a << " : " << msg << std::endl;
 }
 
 template<typename T>
@@ -34,6 +34,17 @@ int main(int argc, char* argv[])
  //   log(std::chrono::system_clock::now(), "main");
 
     std::cout << "Hello World\n";
+
+
+	int x = 100;
+
+	[=]() { std::cout << "no mutable : " << x << std::endl; } ();
+
+	[=]() mutable { x = 300; std::cout << "mutable : " << x << std::endl; x = 200; } ();
+
+	std::cout << x << std::endl;
+
+	getchar();
 
     return EXIT_SUCCESS;
 }
