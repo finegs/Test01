f3673054413a5d6eb2a05f306b930c4ca2849746
KANGSOONGOO <finegs@gmail.com>
2019-10-21 21:48:48 +0900

    bitwise operator

--- pjt02/b.cpp
+++ pjt02/b.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 
+
 using namespace std;
+
+
 int main(int argc, char* argv[])
 {
+
+if (argc > 0 )
+{
+	for(int i = 0;i<argc;i++) 
+		cout << "argv[" << i << "] = " << argv[i] << ((i < argc-1) ? " ," : "");
+}
+
+	cout << endl;
+	int a;
+	if(argc > 1) {
+		a = atoi(argv[1]);
+	} else {
+		cout << "Enter a : "; std::cout.flush();
+		cin >> a;
+		cin.clear();
+	}
+	cout << "a is : " << a << endl;
+	cout << "a & 0xFF : " << (a & 0xFF) << endl;
+	cout << "a | 0xFF : " << (a | 0xFF) << endl;
+	cout << "a ^ 0xFF : " << (a ^ 0xFF) << endl;
+	cout << "a ^ 0x00 : " << (a ^ 0x00) << endl;
+	cout << "a << 1 : " << (a << 1) << endl;
+	cout << "a >> 1 : " << (a >> 1) << endl;
+	cout << "a ^ a : " << (a ^ a ) << endl;
+	for(int i = 31;i>=0;i--)
+//		cout << (a&(0x01<<(32-i))) << "";
+//		cout << ((a<<i)&(0x01<<i)) << "";
+		cout << ((a & (1<<i)) ? "1" : "0") << (i > 3 &&(i%8==0) ? " " : "");
+	cout << endl;
+	cout << "~a : " << (~a) << endl;
     cout << "Hello World" << endl;
     return 0;
 }
