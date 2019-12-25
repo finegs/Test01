593256eedc66a2e4ef06aabd2d2fddde245673d1
finegs <finegs@gmail.com>
2019-12-25 23:18:24 +0900

    aaa

--- pjt02/mutil.hpp
+++ pjt02/mutil.hpp
@@ -1,6 +1,8 @@
 #ifndef _MUTIL_HPP_
 #define _MUTIL_HPP_
 
+#include <string>
+
 char* mstrtok(char* _str, const char* _delim); 
 int   mstrcmp(const char* _str1, const char* _str2);
 std::string getTimestamp();
