af98d21cee65a7e2ea8c1f1d8da615d16a452d28
finegs <finegs@gmail.com>
2020-01-12 07:50:07 +0900

    memcpy with byte size

--- pjt02/aa.cpp
+++ pjt02/aa.cpp
@@ -18,10 +18,68 @@
 
 using namespace std;
 
+void buildMaxHeap(int arr[], int n)  
+{  
+    for (int i = 1; i < n; i++)  
+    { 
+        // if child is bigger than parent 
+        if (arr[i] > arr[(i - 1) / 2])  
+        { 
+            int j = i; 
+      
+            // swap child and parent until 
+            // parent is smaller 
+            while (arr[j] > arr[(j - 1) / 2])  
+            { 
+                swap(arr[j], arr[(j - 1) / 2]); 
+                j = (j - 1) / 2; 
+            } 
+        } 
+    } 
+} 
+  
+void heapSort(int arr[], int n)  
+{ 
+    buildMaxHeap(arr, n); 
+  
+    for (int i = n - 1; i > 0; i--) 
+    { 
+        // swap value of first indexed  
+        // with last indexed  
+        swap(arr[0], arr[i]); 
+      
+        // maintaining heap property 
+        // after each swapping 
+        int j = 0, index; 
+          
+        do
+        { 
+            index = (2 * j + 1); 
+              
+            // if left child is smaller than  
+            // right child point index variable  
+            // to right child 
+            if (arr[index] < arr[index + 1] && 
+                                index < (i - 1)) 
+                index++; 
+          
+            // if parent is smaller than child  
+            // then swapping parent with child  
+            // having higher value 
+            if (arr[j] < arr[index] && index < i) 
+                swap(arr[j], arr[index]); 
+          
+            j = index; 
+          
+        } while (index < i); 
+    } 
+} 
+
+#if 0
 void buildMaxHeap(int arr[], int n)
 {
 	for(int i = 1;i<n;i++) {
-		if(arr[i]>arr[i-1]/2) {
+		if(arr[i]>arr[(i-1)/2]) {
 			int j = i;
 
 			while(arr[j]>arr[(j-1)/2]) {
@@ -56,6 +114,8 @@ void heapsort(int arr[], int n)
 		} while(index < i);
 	}
 }
+
+#endif
 
 int main(int argc,char* argv[]) {
 	unordered_map<string, string> argm;
@@ -113,6 +173,7 @@ int main(int argc,char* argv[]) {
 			char* token= __STRTOK(temp, delim);
 			while(token) {
 				args.push_back(std::string(token));
+				*(token+strlen(token)) = delim[0];
 				token = __STRTOK(NULL, delim);
 			}
 
@@ -172,14 +233,17 @@ int main(int argc,char* argv[]) {
 		int *arr;
 		int arrLen = 10;
         int n;
-        if(a) {
+		printf("-a is %s\n", ss.c_str());
+
+        if(!ss.empty()) {
 			arr = (int*)malloc(sizeof(int)*arrLen);
+			memset(arr, 0, arrLen);
+
 			int num;
 			char *ta = mstrtok(a,",");
 			n = 0;
 			printf("\n");
 			while(ta) {
-				num = atoi(ta);
 				if (n >= arrLen) {
 				  int *tarr =
 					  (int *)malloc(sizeof(int) * arrLen * 2);
@@ -188,37 +252,43 @@ int main(int argc,char* argv[]) {
 					exit(-100);
 				  }
 				  memset(tarr, 0, arrLen * 2);
-				  memcpy(tarr, arr, arrLen);
+				  memcpy(tarr, arr, sizeof(int)*arrLen);
+
 				  free(arr);
 				  arr = NULL;
+
 				  arrLen = arrLen * 2;
 				  arr = tarr;
 				  tarr = NULL;
 				}
+				num = atoi(ta);
+				printf("%s[%d]=%d", (n>0?", ":""), n, num);
 				arr[n++] = num;
-				printf("%d\t", num);
 				ta=mstrtok(NULL, ",");
 			}
-			printf("\n");
+			printf("\nn=%d\n", n);
 		}
 		else {
-			std::vector<int> iv{10,20,15,27,9,21};
+			static std::vector<int> iv{10,20,15,27,9,21,88,55,33,99,1001,12,1,2,-100,-201};
 			// arr = {10,20, 15, 27, 9, 21};
 			arr = &iv[0];
-            n = 6;
+            n = iv.size();
         }
 
      	// int n = sizeof(arr)/sizeof(arr[0]);
 		
 		printf("before:");
-		for(int i = 0;i<n;i++) printf("%s%d", (i>0?", ":""), arr[i]);
+		for(int i = 0;i<n;i++) printf("%s%d", (i>0?", " : ""), arr[i]);
 		printf("\n");
 
-		heapsort(arr, n);
+		heapSort(arr, n);
 
 		printf("after:");
 		for(int i = 0;i<n;i++) printf("%s%d", (i>0?", ":""), arr[i]);
 		printf("\n");
+
+		if(a) a = NULL;
+		if(arr) free(arr);
 
 	}
 	else {
