8f05475016a192f152279d95b2ac989f60291ff2
SoonGoo Kang <finegs@gmail.com>
2020-07-25 07:00:24 +0900

    Debug, Release, my.cpp added

---
+++ pjt05/my.hpp
@@ -0,0 +1,15 @@
+#ifndef __MY_HPP__
+#define __MY_HPP__
+
+#include <iostream>
+#include <chrono>
+
+namespace my {
+	class TimeStamp {
+		friend std::ostream& operator<<(std::ostream& os, const TimeStamp& o);
+	};
+
+	inline TimeStamp ts() { return TimeStamp(); }
+}
+
+#endif
