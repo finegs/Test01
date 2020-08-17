5965aebd42a45f9d07c9ff93b4c7c1fe1078bdec
SoonGoo Kang <finegs@gmail.com>
2020-08-17 14:58:31 +0900

    ....

--- pjt05/a/t.cpp
+++ pjt05/a/t.cpp
@@ -1,4 +1,5 @@
 
+#if 0
 #include <iostream>
 #include <map>
 
@@ -10,14 +11,6 @@ int main(int argc, char* argv[]) {
 	multimap<int, int> mm;
 
 	mm.insert({10,1});
-	mm.insert({30,3});
-	mm.insert({40,4});
-	mm.insert({70,7});
-	mm.insert({60,6});
-	mm.insert({30,3});
-	mm.insert({20,2});
-	mm.insert({80,8});
-	mm.insert({90,9});
 	mm.insert({40,11});
 	mm.insert({70,17});
 
@@ -43,8 +36,81 @@ int main(int argc, char* argv[]) {
 	for(iter = mm.begin();iter != mm.end();iter++) {
 		cout << "[" << iter->first << "," << iter->second << "] ";
 	}
-
 	return 0;
 }
 
+#endif
 
+#include <cstdlib>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+namespace my {
+	template<typename T>
+	void Test_Map(size_t N)
+	{
+		T dict;
+		for (int i = 0; i < N; i++)
+		{
+			dict.insert({i, i});
+		}
+		for (int i = 0; i < N; i++)
+		{
+			dict.find(i);
+		}
+	};
+}
+
+class FactorMaker {
+    map<int, double> cache;
+
+    double longRunningFetch(int key)
+    {
+        const double factor = static_cast<double> (rand()) / RAND_MAX;
+        cout << "calculating factor for key " << key << endl;
+        // lock
+        // cache.insert(make_pair(key, factor));
+        cache.insert({key, factor});
+        // unlock
+        return factor;
+    }
+
+public:
+    double getFactor(int key) {
+        // lock
+        map<int, double>::iterator it = cache.find(key);
+        // unlock
+        return (cache.end() == it) ? longRunningFetch(key) : it->second;
+    }
+};
+
+FactorMaker & getFactorMaker()
+{
+    static FactorMaker instance;
+    return instance;
+}
+
+class UsesFactors {
+public:
+    UsesFactors() {}
+
+    void printFactor(int key) const
+    {
+        cout << getFactorMaker().getFactor(key) << endl;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    const UsesFactors obj;
+
+    for (int i = 0; i < 10; ++i)
+        obj.printFactor(i);
+
+    for (int i = 0; i < 10; ++i)
+        obj.printFactor(i);
+
+    return EXIT_SUCCESS;
+}
