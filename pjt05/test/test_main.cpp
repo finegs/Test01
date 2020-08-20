5a17ca81753ffb1c321c064eea69e0a7f4164999
KANGSOONGOO <finegs@gmail.com>
2020-08-21 06:14:37 +0900

    async queue in c++

--- pjt05/test/test_main.cpp
+++ pjt05/test/test_main.cpp
@@ -1,9 +1,17 @@
+#if 0
 
 #include <iostream>
 #include <cstdio>
 #include <map>
 #include <my.hpp>
 
+
+template<typename T, typename... Types>
+void print(T firstArg, Types... args) {
+	std::cout << firstArg << ' ';
+	print(args...);
+	std::cout << '\n';;
+}
 
 using namespace std;
 using namespace my;
@@ -44,7 +52,7 @@ int main(int argc, char* argv[]) {
 	mm.insert({40,3});
 
 	cout << my::ts() << "mm.size():"<<mm.size()<<endl;
-	cout <<  my::ts() << "mm.count(40):"<<mm.count(40)<<endl;
+	cout << my::ts() << "mm.count(40):"<<mm.count(40)<<endl;
 
 
 	for(iter = mm.begin();iter != mm.end();iter++) {
@@ -53,9 +61,154 @@ int main(int argc, char* argv[]) {
 
 	cout << "\n";
 
+	string ss("Welcome to ");
+
+	print(ss, "C++", 2011, '\n');
+
 	std::getchar();
 
 	return 0;
 }
 
 
+void print(T t, typename... Types) {
+	std::cout << t << ', ';
+	print(args...);
+	std::cout << '\n';
+}
+
+#endif
+
+#if 1
+
+#include <iostream>
+#include <string>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+#include <thread>
+#include <sstream>
+#include <atomic>
+
+class MyInst {
+	private:
+	MyInst(){
+		run.store(true);
+		pause.store(false);
+	}
+
+	std::mutex mtx;
+	std::condition_variable cv;
+	std::queue<std::string> queue;
+	static std::mutex s_mtx;
+	static MyInst* inst;
+
+	public:
+	static MyInst& getInstance(){
+		// std::lock_guard<std::mutex> lg(*mtx);
+		// if(inst == NULL) inst = new MyInst();
+		// return inst*;
+		if(inst != NULL) return *inst;
+		std::lock_guard<std::mutex> lg(s_mtx);
+		if(inst == NULL) inst = new MyInst();
+		return *inst;
+	}
+	std::atomic_bool run;
+	std::atomic_bool pause;
+
+	void push(std::string msg);
+	std::string pop();
+	void clearQueue();
+};
+
+
+void MyInst::push(std::string msg) {
+	std::unique_lock<std::mutex> lk(mtx);
+
+	cv.wait(lk, [&]() { return queue.size() < 10; });
+
+	queue.push(msg);
+
+	cv.notify_all();
+}
+
+std::string MyInst::pop() {
+	std::unique_lock<std::mutex> lk(mtx);
+	cv.wait(lk, [&] () { return !queue.empty(); });
+
+	std::string msg = queue.front();
+	queue.pop();
+	// lk.unlock();
+	cv.notify_all();
+	return msg;
+}
+
+
+void MyInst::clearQueue() {
+
+	if(queue.empty()) return;
+	std::unique_lock<std::mutex> lk(mtx);
+
+	std::queue<std::string> eq;
+	std::swap(queue, eq);
+	// lk.unlock();
+	// cv.notify_all();
+}
+
+using namespace std::literals::chrono_literals;
+
+std::mutex MyInst::s_mtx;
+MyInst* MyInst::inst;
+
+int main() {
+
+	MyInst::getInstance();
+
+	std::thread prod([] { 
+		int i = 0;
+		while(MyInst::getInstance().run) {
+			  std::this_thread::sleep_for(3s); 
+			  if(MyInst::getInstance().pause) 
+			  	continue; 
+			  MyInst::getInstance().push("a_" + i++);
+		 }
+	});
+	std::thread cons([] { 
+		int i = 0; 
+		while(MyInst::getInstance().run) { 
+			std::cout << "con : " << i++ << " : " << MyInst::getInstance().pop() << "\n"; 
+		}
+	});
+
+	std::string line, cmd, word;
+	std::stringstream ss;
+	while(MyInst::getInstance().run) {
+		std::cout << "Command>>" << "\n";
+		std::cin >> line;
+
+		ss.str(""); ss.str(line);
+		ss >> cmd;
+
+		if("stop" == cmd) {
+			MyInst::getInstance().run.store(false);
+		}
+		else if("pause" == cmd) {
+			MyInst::getInstance().pause.store(true);
+		}
+		else if("resume" == cmd) {
+			MyInst::getInstance().pause.store(false);
+		}
+		else if ("push" == cmd) {
+			ss >> word;
+			MyInst::getInstance().push(word);
+		}
+		else if ("clear" == cmd) {
+			MyInst::getInstance().clearQueue();
+		}
+
+		// std::cout << "main is sleeping ..." << std::endl;
+		// std::this_thread::sleep_for(10s);
+	}
+}
+#endif
