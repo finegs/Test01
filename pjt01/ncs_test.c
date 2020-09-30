80bae7c326d447ef230f3b45c8de63d3fa5e5ecc
SoonGoo Kang <finegs@gmail.com>
2020-09-30 14:59:40 +0900

    ncs_test.c added for ncurses test, \

---
+++ pjt01/ncs_test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ncurses.h>
+
+int main(int argc, char* argv[]) {
+
+	if(argc > 0) {
+		fprintf(stdout, "### %s is started\n", argv[0]);
+	}
+
+	while(true) {
+
+		initscr();
+		noecho();
+		curs_set(FALSE);
+
+		usleep(1000*1000*5);
+	}
+
+	endwin();
+
+
+	return 0;
+}
