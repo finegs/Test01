c3254e6c11fa5b7237d894eab425550d46f7c9dc
KANGSOONGOO <finegs@gmail.com>
2023-01-28 22:44:00 +0900

    idc

--- rpjt2/src/main.rs
+++ rpjt2/src/main.rs
@@ -1,27 +1,106 @@
+<<<<<<< HEAD
 use std::{env, fmt, fmt::Display, process};
 
 struct Config { query: String,
     file_path : String, ignore_case: bool
+=======
+
+use std::collections::HashMap;
+
+#[derive(Clone)] // we'll be cloning it later on
+struct Node<'a> {
+    data: &'a i32 
+}
+
+
+struct Test<'a> {
+    hash_map: HashMap<&'a str, Node<'a>>  // the hash map owns the struct
+}
+
+impl<'a> Test<'a> {
+    fn new() -> Test<'a> {
+        Test {hash_map: HashMap::new()}
+    }
+
+    fn join(
+        &mut self, // must be mutable
+        node: Node<'a>) { // do not pass a reference
+        self.hash_map.insert("test", node);  // inserting moves `node`
+    }
+}
+
+fn main() {
+    let stuff = Node {data: &12};
+    let mut test = Test::new();
+
+    test.join(stuff.clone());  // if we don't clone, `stuff` will get moved
+
+    println!("{}", *test.hash_map["test"].data);  // outputs "12"
+}
+
+/*
+use std::collections::HashMap;
+
+// pub type OptionMap = HashMap<String, Option<String>>;
+pub type OptionMap = HashMap<String, Option<String>>;
+
+fn test_option_map<'a>(map : &'a OptionMap, key : &'a String) {
+    match map.get(key) {
+        Some(ref v) => {
+            match &v {
+                 &&&Some(ref sval) => println!("Value found : key = {}, value = {}", key, sval),
+                 &&&None => eprintln!("No value for key : {}", key)
+            }
+        },
+        None => {
+            eprintln!("Value not found. key={}", key);
+        }
+    }
+}
+fn main() {
+    let mut ex  = OptionMap::new();
+
+    let key = "test".to_string();
+    ex.insert(key.clone(), None);
+
+    test_option_map(&ex, &key);
+
+    ex.insert(key.clone(), Some("Good".to_string()));
+
+    test_option_map(&ex, &key);
+}
+
+*/
+
+/*
+use std::{env, fmt, fmt::Display};
+struct Config {
+    query: String,
+    file_path : String,
+    ignore_case: bool,
+>>>>>>> 0642e2d (idc)
 }
 
 impl Display for Config {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> fmt::Result {
-       f.write_fmt(format_args!("query:{}, file_path:{}, ignore_case:{}", self.query, self.file_path, self.ignore_case))
+       f.write_fmt(format_args!("query:{}, file_path:{}, ignore_case:{}"
+               , self.query
+               , self.file_path
+               , self.ignore_case))
     }
 }
 
 impl Config {
      pub fn build(
-         mut args: impl Iterator<Item = String>,
+         args: impl Iterator<Item = String>,
      ) -> Result<Config, &'static str> {
-         args.next();
-
-         let query = match args.next() {
+        let mut args_iter = args.skip(1);
+        let query = match args_iter.next() {
              Some(arg) => arg,
              None => return Err("Didn't get a query string"),
          };
 
-         let file_path = match args.next() {
+         let file_path = match args_iter.next() {
              Some(arg) => arg,
              None => return Err("Didn't get a file path"),
          };
@@ -74,6 +153,7 @@ fn main() {
         }
     }
 
+<<<<<<< HEAD
     match output {
         Ok(output_file) => {
             println!("Output file : {}", output_file);
@@ -81,6 +161,12 @@ fn main() {
         Err(err_msg) => {
             eprintln!("{}", err_msg);
         }
+=======
+    match Config::build(args.clone().into_iter()) {
+        Ok(cnf) => println!("### query:{}, file_path:{}, ignore_case:{}", 
+                            cnf.query, cnf.file_path, cnf.ignore_case),
+        Err(_) => eprintln!("Error : "),
+>>>>>>> 0642e2d (idc)
     }
 
     // let mut args: Vec<String> = env::args().collect();
@@ -94,3 +180,6 @@ fn main() {
     // args = args.into_iter().map(|arg| arg.to_ascii_lowercase()).collect();
     // args.iter().for_each(|s| println!("{s}"));
 }
+
+*/
+
