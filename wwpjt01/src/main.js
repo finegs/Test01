5906975dd6190cc3f1ac74bbc50b9cac31408797
finegs <finegs@gmail.com>
2023-07-02 11:18:07 +0900

    todo init

---
+++ wwpjt01/src/main.js
@@ -0,0 +1,46 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e)=> {
+	e.preventDefault();
+	console.log("button clicked");
+	formValidation();
+});
+
+let formValidation = ()=>{
+	if(input.value === "") {
+		msg.innerHTML = "Post cannot be blank";
+		console.log("failure");
+	}
+	else {
+		console.log("success");
+		msg.innerHTML = "";
+
+		acceptData();
+	}
+}
+
+let data = {}
+let acceptData = () => {
+	data["text"] = input.value;	
+	console.log("data pushed", data);
+	createPost();
+}
+
+
+let createPost = ()=> {
+	// posts.innerHTML = data.text;
+	posts.innerHTML += `
+	<div>
+		<p>${data.text}</p>
+		<span class="options">
+			<i class="fas fa-edit"></i>
+			<i class="fas fa-trash-alt"></i>
+		</span>
+	</div>
+	`;
+
+	input.value = "";
+}
