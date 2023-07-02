4efc9664c802699452388b596f5282e178619f40
Finegs on WSL <finegs@gmail.com>
2023-07-02 20:15:51 +0900

    todo app with basic crud html

--- wwpjt01/src/main.js
+++ wwpjt01/src/main.js
@@ -36,11 +36,19 @@ let createPost = ()=> {
 	<div>
 		<p>${data.text}</p>
 		<span class="options">
-			<i class="fas fa-edit"></i>
-			<i class="fas fa-trash-alt"></i>
+			<i onClick="editPost(this)" class="fas fa-edit"></i>
+			<i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
 		</span>
 	</div>
 	`;
 
 	input.value = "";
+}
+
+let deletePost = (e) => {
+	e.parentElement.parentElement.remove();
+}
+
+let editPost = (e) => {
+	input.value = e.parentElement.previousElementSibling.innerHTML;
 }
