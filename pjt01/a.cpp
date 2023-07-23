ddb125f6a399ddbcae3aae6dc0c56cdfbc2df409
Finegs on WSL <finegs@gmail.com>
2023-07-23 11:57:57 +0900

    aa

--- pjt01/a.cpp
+++ pjt01/a.cpp
@@ -1,92 +1,62 @@
+#include <cstddef>
+#include <cstdint>
 #include <cstdio>
+#include <cstdlib>
 #include <iostream>
+#include <ostream>
+#include <unordered_map>
+#include <functional>
 
-struct DynArrayQueue {
-    int front, rear;
-    int capacity;
-    int padd;
-    int *array;
+struct Node {
+	char *name;
+	uint64_t value;
+	friend std::ostream& operator<<(std::ostream&, const Node&);
 };
 
-void resizeQueue(DynArrayQueue* queue);
-
-struct DynArrayQueue *createDynQueue() {
-   DynArrayQueue* q = new DynArrayQueue(); 
-   if(!q) return NULL;
-   q->capacity = 1;
-   q->front = q->rear = 1;
-   q->array = new int[(size_t)q->capacity];
-   if(!q->array) return NULL;
-   return q;
+std::ostream& operator<<(std::ostream& os, const Node& o){
+	os << '{' << o.name << ',' << o.value << '}';
+	return os;
 }
 
-int isEmptyQueue(DynArrayQueue* q) {
-    return q->front == -1;
+template <>
+struct std::hash<const char*> {
+	size_t operator()(const char* str) const {
+		size_t h = 5381;
+		for(const char c = *str;c;str++) {
+			h = h<<5 & h;
+		}
+		return h;	 
+	}
+};
+
+template<>
+struct std::hash<const Node> {
+	size_t operator()(const Node &o) const {
+		return std::hash<const char*>()(o.name);
+	}
+};
+
+template<typename K, typename V>
+std::ostream& operator<<(std::ostream& os, const std::unordered_map<K,V>& m) {
+	char sep[3]{'\0', ',','\0'};
+	for(auto& e : m) {
+		os<< sep << "{" << e.first << ", " << e.second << "}";
+		sep[0] = ' ';
+	}
+	return os;
 }
 
-int isFullQueue(DynArrayQueue* q) { 
-    return (q->rear+1)%q->capacity == q->front;
-}
-
-int queueSize(DynArrayQueue* q) {
-    return ((q->capacity-q->front+q->rear+1)%q->capacity);
-}
-
-void enqueue(DynArrayQueue* q, int data) {
-    if(isFullQueue(q))
-        resizeQueue(q);
-    q->rear = (q->rear+1)%q->capacity;
-    q->array[q->rear] = data;
-    if(q->front == -1)
-        q->front = q->rear;
-}
-
-void resizeQueue(DynArrayQueue* q) {
-    int size = q->capacity;
-    q->capacity = q->capacity * 2;
-    q->array = (int*)realloc((void*)q->array, (size_t)q->capacity);
-    if(!q->array) {
-        printf("Memory Error, %s:%d\n", __FILE__, __LINE__);
-        return;
-    }
-
-    if(q->front > q->rear) {
-        for(int i = 0;i < q->front;i++) {
-            q->array[i+size] = q->array[i];
-        }
-        q->rear = q->rear+size;
-    }
-}
-
-int queueFront(DynArrayQueue* q) {
-    if(isEmptyQueue(q)) return -1;
-    return q->array[q->front];
-}
-
-void printQueue(FILE* fp, DynArrayQueue* q) {
-    for (int i = q->front; i < (q->rear); i=((i+1)%q->capacity)) {
-        fprintf(fp, "[%d]=%d\n", i, q->array[i]);
-    }
-}
-
-int main() {
+int main(int argc, char* argv[]) {
 
 	using namespace std;
-    cout << "###0###" << std::endl;
-    DynArrayQueue* q = createDynQueue();
-    enqueue(q, 1);
-    enqueue(q, 2);
-    cout << "###1###" << std::endl;
-    enqueue(q, 3);
-    enqueue(q, 4);
-    cout << "###2###" << std::endl;
-    enqueue(q, 5);
-    enqueue(q, 6);
-    
-    cout << "###3###" << std::endl;
 
-    cout << "###4 ### : size(queue) : " << queueSize(q) << std::endl;;
-    printQueue(stdout, q);
+	unordered_map<const char*, Node> nmap;
 
-    return 0;
+	for (int i = 1; i < argc; i++) {
+		nmap.insert({argv[i], {.name = argv[i], .value = (uint64_t)i*100}});
+	}
+
+	std::cout << "Map is " << std::endl;
+
+	return EXIT_SUCCESS;
 }
