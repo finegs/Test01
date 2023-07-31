1736d798c2c51124263fec3f6b390a12456276d4
S.G <finegs@gmail.com>
2023-07-31 18:22:07 +0900

    ~/.tmux.conf added for TMux Plugin Manager

--- pjt01/a.cpp
+++ pjt01/a.cpp
@@ -2,117 +2,112 @@
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
+#include <functional>
 #include <iostream>
 #include <ostream>
 #include <unordered_map>
-#include <functional>
+#include <sstream>
 
 struct Node {
-	char *name;
-	uint64_t value;
-	friend std::ostream& operator<<(std::ostream&, const Node&);
+  char *name;
+  uint64_t value;
+  friend std::ostream &operator<<(std::ostream &, const Node &);
 };
 
-enum LEN { NODE_BUFFER = 1024*1024};
-static Node NODEBUFFER[1024*1024];
+enum LEN { NODE_BUFFER = 1024 * 1024 };
+static Node NODEBUFFER[1024 * 1024];
 static size_t nodeBufferIdx = 0;
 
-Node* newNode() { return &NODEBUFFER[nodeBufferIdx++]; }
-
-std::ostream& operator<<(std::ostream& os, const Node& o){
-	os << '{' << o.name << ',' << o.value << '}';
-	return os;
+static Node* newNode() { 
+	return &NODEBUFFER[nodeBufferIdx++]; 
 }
 
-template <>
-struct std::hash<const char*> {
-	size_t operator()(const char* str) const {
-		size_t h = 5381;
-		char c = '\0';
-		for(c=str[0];(c = str[0]);str++) {
-			h = (h<<5) & c;
-		}
-		return h;	 
-	}
-};
-
-template<>
-struct std::hash<const Node> {
-	size_t operator()(const Node &o) const {
-		return std::hash<const char*>()(o.name);
-	}
-};
-
-template<typename K, typename V> std::ostream& operator<<(std::ostream& os, const std::unordered_map<K,V>& m) {
-	char sep[3]{'\0', ',','\0'};
-	os << "{";
-	for(auto& e : m) {
-		os<< sep << "{" << e.first << ", " << e.second << "}";
-		sep[0] = ' ';
-	}
-	os << "}";
-	return os;
+std::ostream &operator<<(std::ostream &os, const Node &o) {
+  os << '{' << o.name << ',' << o.value << '}';
+  return os;
 }
 
-int main(int argc, char* argv[]) {
+template <> struct std::hash<const char *> {
+  size_t operator()(const char *str) const {
+    size_t h = 5381;
+    char c = '\0';
+    for (c = str[0]; (c = str[0]); str++) {
+      h = (h << 5) & c;
+    }
+    return h;
+  }
+};
 
-	using namespace std;
+template <> struct std::hash<const Node> {
+  size_t operator()(const Node &o) const {
+    return std::hash<const char *>()(o.name);
+  }
+};
 
-	unordered_map<const char*, Node> nmap;
+template <typename K, typename V>
+std::ostream &operator<<(std::ostream &os, const std::unordered_map<K, V> &m) {
+  char sep[3]{'\0', ',', '\0'};
+  os << "{";
+  for (auto &e : m) {
+    os << sep << "{" << e.first << ", " << e.second << "}";
+    sep[0] = ' ';
+  }
+  os << "}";
+  return os;
+}
 
-	for (int i = 1; i < argc; i++) {
-		nmap.insert({argv[i], {.name = argv[i], .value = (uint64_t)i}});
-	}
+int main(int argc, char *argv[]) {
 
-	std::cout << "Map : " << nmap << std::endl;
+  using namespace std;
 
+  unordered_map<const char *, Node> nmap;
 
-	char cmd;
-	for(std::string line; std::getline(std::cin, line);) {
-		cmd << line;
+  for (int i = 1; i < argc; i++) {
+    nmap.insert({argv[i], {.name = argv[i], .value = (uint64_t)i}});
+  }
 
-		switch (cmd) {
-			// Add
-			case '+':
-				{
-					Node newNode = newNode();
+  std::cout << "Map : " << nmap << std::endl;
 
-				}
-				break;
-			case '-':
-				// Remove
-				{
-				}
-				break;
-			case '=':
-				// Update
-				{
-				}
-				break;
-			case '*':
-				// Show_all
-				{
-				}
-				break;
-			case '@':
-				// get
-				{
-				}
-				break;
-			case '~':
-				// Clear
-				{
-				}
-				break;
-			default:
-				{
-					std::cout << "Unsupported Command : " << cmd << std::endl;
-					break;
-				}
-		}
-		}
-	}
-	
+  char cmd;
+  for (std::string line; std::getline(std::cin, line);) {
+	stringstream sstream;
+	sstream.str(line);
+	sstream >> cmd;
+    /* cmd << line; */
 
-	return EXIT_SUCCESS;
+    switch (cmd) {
+    // Add
+    case '+': {
+      Node* node = newNode();
+
+	  delete(node);
+    } 
+	break;
+    case '-':
+      // Remove
+      {}
+      break;
+    case '=':
+      // Update
+      {}
+      break;
+    case '*':
+      // Show_all
+      {}
+      break;
+    case '@':
+      // get
+      {}
+      break;
+    case '~':
+      // Clear
+      {}
+      break;
+    default: {
+      std::cout << "Unsupported Command : " << cmd << std::endl;
+      break;
+    }
+    }
+  }
+  return EXIT_SUCCESS;
 }
