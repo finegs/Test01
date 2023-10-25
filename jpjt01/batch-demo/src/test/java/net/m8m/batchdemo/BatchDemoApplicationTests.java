0751588b1176888999bc16e5443214149a35d152
KANGSOONGOO <finegs@gmail.com>
2023-10-25 16:22:09 +0900

    gradle with unit test

--- jpjt01/batch-demo/src/test/java/net/m8m/batchdemo/BatchDemoApplicationTests.java
+++ jpjt01/batch-demo/src/test/java/net/m8m/batchdemo/BatchDemoApplicationTests.java
@@ -7,18 +7,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-// @SpringBootTest
+ // @SpringBootTest
  @TestMethodOrder(OrderAnnotation.class)
-// @TestPropertySource(properties="app.scheduling.enable=false")
 class BatchDemoApplicationTests {
 
 		List<String> arr = Arrays.asList("aa","bbb", "ccc cc");
--- jpjt01/mybatch/src/test/java/com/example/mybatch/DemoApplicationTests.java
+++ jpjt01/mybatch/src/test/java/com/example/mybatch/DemoApplicationTests.java
@@ -3,18 +3,25 @@ package com.example.mybatch;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
 class DemoApplicationTests {
 
+
 	@Test
+	@Order(1)
 	void contextLoads() {
 		System.out.println("contextLoads is executed");
 	}
 
 	@Test
+	@Order(2)
 	void testFalse() {
 		int a = 1;
 		int b = 2;
@@ -22,6 +29,7 @@ class DemoApplicationTests {
 	}
 
 	@Test
+	@Order(3)
 	void testTrue() {
 		int a = 1;
 		int b = 1;
