be3ee57382a36de588d397fd4a183a99b2d9c247
finegs <finegs@gmail.com>
2023-12-02 20:58:02 +0900

    spring batch with quartz

--- jpjt01/batch-demo/src/main/java/net/m8m/batchdemo/BatchDemoConfig.java
+++ jpjt01/batch-demo/src/main/java/net/m8m/batchdemo/BatchDemoConfig.java
@@ -4,18 +4,22 @@ package net.m8m.batchdemo;
 import javax.sql.DataSource;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @EnableBatchProcessing
 @Configuration
 public class BatchDemoConfig {
-	@Autowired
-	private DataSource jobBuilderFactory;
 
 	@Autowired
-	private StepBuilder stepBuilder;
+	private DataSource dataSource;
 
+	@Autowired
+	private JobRepository jobRepository;
+
+	@Autowired
+	private PlatformTransactionManager platformTransanctionManager;
 
 }
