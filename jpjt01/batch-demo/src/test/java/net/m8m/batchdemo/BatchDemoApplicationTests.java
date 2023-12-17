105d7a17d076ed5c5d5cdc94dfd34324600a7ad4
finegs <finegs@gmail.com>
2023-12-17 14:21:25 +0900

    batch-demo with stream test

--- jpjt01/batch-demo/src/test/java/net/m8m/batchdemo/BatchDemoApplicationTests.java
+++ jpjt01/batch-demo/src/test/java/net/m8m/batchdemo/BatchDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package net.m8m.batchdemo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -19,16 +20,16 @@ import lombok.extern.slf4j.Slf4j;
  @TestMethodOrder(OrderAnnotation.class)
 class BatchDemoApplicationTests {
 
-		List<String> arr = Arrays.asList("aa","bbb", "ccc cc");
+	List<String> arr = Arrays.asList("aa","bbb", "ccc cc");
 
 	@Test
 	@Order(1)
 	void contextLoads() {
-		assertTrue(true, "contextLoads");	
+	assertTrue(true, "contextLoads");	
 		// List<String> arr = Arrays.asList("aa","bbb", "ccc cc");
 		// AtomicInteger idx = new AtomicInteger(0);
 		//
-	// arr.stream().filter(s->  s.contains("aa")).forEach(s -> System.out.printf("[%d]:%s\n", idx.getAndIncrement(), s));
+		// arr.stream().filter(s->  s.contains("aa")).forEach(s -> System.out.printf("[%d]:%s\n", idx.getAndIncrement(), s));
 		// int expectedValue = 1;
 		// assertTrue(idx.get() == expectedValue, 
 		// 		String.format("filtered expected : %d, value : %d", expectedValue, idx.get()));
@@ -38,7 +39,9 @@ class BatchDemoApplicationTests {
 	@Order(2)
 	void test_stream_filter() {
 		AtomicInteger idx = new AtomicInteger(0);
-		arr.stream().filter(s->  s.contains("aa")).forEach(s -> System.out.printf("[%d]:%s\n", idx.getAndIncrement(), s));
+		arr.stream()
+			.filter(s->  s.contains("aa"))
+			.forEach(s -> System.out.printf("[%d]:%s\n", idx.getAndIncrement(), s));
 		int expectedValue = 2;
 		assertFalse(idx.get() == expectedValue, 
 				String.format("filtered expected : %d, value : %d", expectedValue, idx.get()));
@@ -63,9 +66,12 @@ class BatchDemoApplicationTests {
 	@Test
 	@Order(5)
 	void stream_test() {
-		List<Integer> aa = Arrays.asList(1,2,3,4);
+		List<Integer> aa = Arrays.asList(1,2,3,4,5);
 
-		var sum_of_aa = aa.stream().reduce(0, (a,b) ->  a + b);
+		var sum_of_aa = aa.stream().map(a->a.intValue()).reduce(0, (a,b) ->  a + b);
+
+		assertEquals(sum_of_aa, 15);
+
 		log.info("stream_test : {}", sum_of_aa);
 	}
 }
