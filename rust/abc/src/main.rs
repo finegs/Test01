1daa65d04532d443b4d95840608786344a80513c
S.G.KANG <finegs@gmail.com>
2024-12-29 21:51:09 +0900

    rust - abc : Rc<RefCell>

--- rust/abc/src/main.rs
+++ rust/abc/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::cell::{RefCell, RefMut};
+use std::collections::HashMap;
+use std::rc::Rc;
+
+pub fn main() {
+    let shared_map = Rc::new(RefCell::new(HashMap::new()));
+
+    {
+        let mut map:  RefMut<'_, _> = shared_map.borrow_mut();
+
+        map.insert("a", 1111);
+        map.insert("b", 1010);
+        map.insert("c", 202020);
+    }
+
+    let total = shared_map.borrow().values().sum::<i64>();
+    println!("total : {}", &total);
 }
+
